dfd6f3e6217f6705c0dd8514a2e5e73e9eb29fdc
TrialAndErrror <coding@trialanderrror.com>
2022-09-02 17:01:18 -0400

    WIP

--- numba/numba_tests.py
+++ numba/numba_tests.py
@@ -1,11 +1,12 @@
 """
 Testing utilities for Numba
 """
-import timeit
+import time
 from numba import jit
+import json
 import numpy as np
-import random
-
+from scipy import stats
+import statistics
 
 """
 Numba is a just-in-time compiler for Python that works best on code that uses NumPy arrays and functions, and loops. 
@@ -15,7 +16,7 @@ instruct Numba to compile them. When a call is made to a Numba-decorated functio
 """
 
 
-HORSES = [
+HORSES = np.array((
     'Epona',
     'Torrent',
     'Roach',
@@ -26,39 +27,113 @@ HORSES = [
     'Invincible',
     'Rapidash',
     'Buck'
-]
-
-WINNERS = {
-    'Epona': 0,
-    'Torrent': 0,
-    'Roach': 0,
-    'Shadowmere': 0,
-    'Chestnut': 0,
-    'Red Hare': 0,
-    'Agro': 0,
-    'Invincible': 0,
-    'Rapidash': 0,
-    'Buck': 0,
-}
+))
 
 
-def simulate_race():
-    horse_scores = []
-
-    for horse in HORSES:
-        horse_scores.append((horse, random.random()))
-
-    horse_scores.sort(key=lambda x: x[1])
-    return horse_scores[0][0]
+def numpy_race():
+    results = np.random.rand(100_000, 10)
+    winner_ids = np.argmax(results,axis=1)
+    champion_id = stats.mode(winner_ids)[0]
+    champion = HORSES[champion_id]
+    return champion
 
 
-def run_test():
-    for num in range(100_000):
-        winner = simulate_race()
-        WINNERS[winner] += 1
+def run_with_timer(num):
+    print(f'Loop Num {num}')
+    start = time.perf_counter()
+    champion = numpy_race()
+    stop = time.perf_counter()
+    print(f'The champion is: {champion}')
+    return stop - start
 
 
-def main():
-    run_test()
+@jit(nopython=True)
+def run_compiled_with_timer(num):
+    print(f'Loop Num {num}')
+    results = np.random.rand(100_000, 10)
+    winner_ids = np.argmax(results, axis=1)
 
-    print(f'The winner is: {max(WINNERS, key=WINNERS.get)}!')
+    freq = {}
+    for i in winner_ids:
+        freq.setdefault(i, 0)
+        freq[i] += 1
+
+    hf = max(freq.values())
+
+    hflst = []
+
+    for i, j in freq.items():
+        if j == hf:
+            hflst.append(i)
+
+    champion_id = hflst[0]
+
+    champion = HORSES[champion_id]
+
+    print(f'The champion is: {champion}')
+
+
+def mode(lst):
+    freq = {}
+    for i in lst:
+        freq.setdefault(i, 0)
+        freq[i] += 1
+
+    hf = max(freq.values())
+
+    hflst = []
+
+    for i, j in freq.items():
+        if j == hf:
+            hflst.append(i)
+
+    return hflst
+
+
+def run_with_numba():
+    start: float = time.perf_counter()
+
+    times = [
+        run_compiled_with_timer(num)
+        for num in range(100)
+    ]
+    stop: float = time.perf_counter()
+
+    print(f'Fastest: {min(times)}')
+    print(f'Slowest: {max(times)}')
+    print('All Times:')
+    print(sorted(times))
+
+    results = {
+        'Fastest': min(times),
+        "Slowest": max(times),
+        "Times": sorted(times)
+
+    }
+
+    with open('numba_results.json', 'w+') as file:
+        json.dump(results, file, indent=2)
+
+
+def run_standard_numpy():
+    times = [run_with_timer(num)
+             for num in range(100)]
+
+    print(f'Fastest: {min(times)}')
+    print(f'Slowest: {max(times)}')
+    print('All Times:')
+    print(sorted(times))
+
+    results = {
+        'Fastest': min(times),
+        "Slowest": max(times),
+        "Times": sorted(times)
+
+    }
+
+    with open('plain_results.json', 'w+') as file:
+        json.dump(results, file, indent=2)
+
+
+if __name__ == '__main__':
+    run_with_numba()
