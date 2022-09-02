c892e8701494577ac13928f4b239660cb59ef448
Wade Green <wade@hirelofty.com>
2022-09-02 11:24:03 -0400

    separate project sides

--- numba/numba_tests.py
+++ numba/numba_tests.py
@@ -1,6 +1,9 @@
 """
 Testing utilities for Numba
 """
+import timeit
+from numba import jit
+import numpy as np
 
 """
 Numba is a just-in-time compiler for Python that works best on code that uses NumPy arrays and functions, and loops. 
@@ -8,3 +11,4 @@ The most common way to use Numba is through its collection of decorators that ca
 instruct Numba to compile them. When a call is made to a Numba-decorated function it is compiled to machine code 
 “just-in-time” for execution and all or part of your code can subsequently run at native machine code speed!
 """
+
